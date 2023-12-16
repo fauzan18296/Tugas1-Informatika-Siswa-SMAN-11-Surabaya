89a5222362492cd0f609ddc3f570c6b416a9e156
fauzan18296 <fauzan18296@gmail.com>
2023-12-16 09:12:14 +0700

    add:deploy Tugas Informatika

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Tugas1-Informatika-Siswa-SMAN-11-Surabaya/",
 });

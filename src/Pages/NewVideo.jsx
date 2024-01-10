99cbe70d790d17beb37601c5a662c225736b1f6d
fauzan18296 <fauzan18296@gmail.com>
2024-01-10 22:07:35 +0700

    feat: menambahkan feature untuk kembali ke page home di bagian content new video

--- src/Pages/NewVideo.jsx
+++ src/Pages/NewVideo.jsx
@@ -2,7 +2,6 @@ import ContentNewVideo from "../Components/Content/ContentNewVideo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-
 const NewVideo = () => {
   return (
     <div

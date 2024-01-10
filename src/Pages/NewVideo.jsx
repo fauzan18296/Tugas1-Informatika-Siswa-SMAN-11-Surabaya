4f109da63c9e71ffc1ca49f77b41942af39860ef
fauzan18296 <fauzan18296@gmail.com>
2024-01-10 21:58:04 +0700

    feat: menambah feature kembali ke page home  di bagian content new video

--- src/Pages/NewVideo.jsx
+++ src/Pages/NewVideo.jsx
@@ -1,25 +1,31 @@
 import ContentNewVideo from "../Components/Content/ContentNewVideo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const NewVideo = () => {
   return (
     <div
-      className="bg-dark"
+      className="bg-dark d-flex justify-content-center align items-center"
       style={{
         letterSpacing: "4px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         flexDirection: "column",
         minHeight: "100vh",
         width: "100%",
         maxWidth: "100%",
       }}
     >
-      <div className="text-center text-white my-4">
+      <div className="d-flex align-items-center justify-content-start ms-4 my-3 fs-1">
+        <Link to="/*" className="text-white">
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </Link>
+      </div>
+
+      <div className="text-center text-white fw-semibold my-4">
         <h1>New Video Aestetic</h1>
       </div>
       <ContentNewVideo />
-      <div className="text-center text-white my-4">
+      <div className="text-center text-white fw-semibold my-4">
         <h1>New Video Meme</h1>
       </div>
       <ContentNewVideo />
